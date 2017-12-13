56345a708a1d0aa34d5f6c9750fcdc8b2a995ff8
Sergii Kabashniuk <skabashniuk@redhat.com>
2017-12-13 17:16:34 +0200

    Added install script

---
+++ scripts/get_che.sh
@@ -0,0 +1,1 @@
+sudo curl -L "https://github.com/skabashnyuk/cli/releases/download/$(curl -L -s -H 'Accept: application/json' https://github.com/skabashnyuk/cli/releases/latest |  sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')/$(echo che_`uname -s`_`uname -m`)" -o /usr/local/bin/che | sudo  chmod +x /usr/local/bin/che
